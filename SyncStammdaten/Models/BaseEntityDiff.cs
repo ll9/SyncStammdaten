0d9322dee5e1783276ab0db4ab919e83e32ec137
ll9 <llwierer@web.de>
2019-02-26 21:50:33 +0100

    base entity diff

---
+++ SyncStammdaten/Models/BaseEntityDiff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncStammdaten.Models
+{
+    public class BaseEntityDiff
+    {
+        [Key]
+        public string Id { get; set; }
+        public int Clock { get; set; }
+        public string Json { get; set; }
+    }
+}
