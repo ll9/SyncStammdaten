2555c5c6e21f7f59be86c771a915cd82c599268f
ll9 <llwierer@web.de>
2019-02-26 21:28:39 +0100

    base model

---
+++ SyncStammdaten/Models/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncStammdaten.Models
+{
+    public class BaseEntity
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Adress { get; set; }
+
+        public bool IsDeleted { get; set; }
+        public bool IsSynced { get; set; }
+        public int Clock { get; set; }
+    }
+}
