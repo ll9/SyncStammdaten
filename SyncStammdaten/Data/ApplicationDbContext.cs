1c5d5845267634492d222416c4c8b60a71b0be6d
ll9 <llwierer@web.de>
2019-02-26 22:16:40 +0100

    initial migration

---
+++ SyncStammdaten/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SyncStammdaten.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncStammdaten.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<BaseEntityDiff> BaseEntityDiffs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(System.Configuration.ConfigurationManager.AppSettings["ConnectionString"]);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ SyncStammdaten/Migrations/20190226211621_Init.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SyncStammdaten.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BaseEntityDiffs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Clock = table.Column<int>(nullable: false),
+                    Json = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BaseEntityDiffs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BaseEntityDiffs");
+        }
+    }
+}
